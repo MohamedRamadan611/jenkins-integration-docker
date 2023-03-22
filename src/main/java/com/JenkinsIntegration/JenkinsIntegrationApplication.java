5549f6fe7667a47aba61e2953994c851e020543b
MohamedRamadan611 <Mohamed.Ramadan61197@gmail.com>
2023-03-22 02:44:34 +0200

    first commit

---
+++ src/main/java/com/JenkinsIntegration/JenkinsIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.JenkinsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegrationApplication.class, args);
+	}
+
+}
